226fd3b39bac74e835e494e717c925751a959a8a
Aleksander Sushchenko <039pro@gmail.com>
2023-04-11 20:59:35 +0200

    create run

--- stocks_products/logistic/serializers.py
+++ stocks_products/logistic/serializers.py
@@ -1,20 +1,26 @@
 from rest_framework import serializers
 
+from stocks_products.logistic.models import Product, StockProduct, Stock
+
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields =['id', 'title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['produkt', 'quantity', 'price']
+
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
-
-    # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['id' ,'address', 'products']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -26,7 +32,8 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        for sp in positions:
+            StockProduct.objects.create(stock=stock, product=sp['product'], price=sp['price'], quantity = sp['quantity'])
         return stock
 
     def update(self, instance, validated_data):
@@ -39,5 +46,6 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        for sp in positions:
+            StockProduct.objects.update_or_create(stock=stock, product=sp['product'], price=sp['price'], quantity = sp['quantity'])
         return stock
